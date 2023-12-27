979f877d8fced34407d1aaecadaf6f8894b82c64
krishanleong <krishanleong@gmail.com>
2023-12-26 23:11:28 -0500

    Start of version 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
-// import App from './App.jsx'
-// import './index.css'
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} className="test" defaultRating={3} />
-    <StarRating maxRating={20} color="blue" size="77" />
+    {<App />}
+    {/* <StarRating maxRating={5} className="test" defaultRating={3} />
+    <StarRating maxRating={20} color="blue" size="77" /> */}
   </React.StrictMode>
 );
